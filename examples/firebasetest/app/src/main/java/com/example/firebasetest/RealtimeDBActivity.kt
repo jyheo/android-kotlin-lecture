4c788abee001a1db9dd309a3bbffc78603b09c24
Junyoung Heo <jyheo0@gmail.com>
2020-10-17 15:12:28 +0900

    Add activity for realtime db test code

---
+++ examples/firebasetest/app/src/main/java/com/example/firebasetest/RealtimeDBActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firebasetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RealtimeDBActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_realtime_db)
+    }
+}
