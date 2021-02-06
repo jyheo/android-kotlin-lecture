b4633847d447044473e6bc3004f500e794b5a91f
Junyoung Heo <jyheo0@gmail.com>
2021-02-06 23:31:57 +0900

    viewmodel, viewbinding

--- examples/activity_intent/app/src/main/java/com/example/activity_intent/MainActivity.kt
+++ examples/activity_intent/app/src/main/java/com/example/activity_intent/MainActivity.kt
@@ -5,38 +5,61 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
+import com.example.activity_intent.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MyViewModel
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         Log.i(TAG, "$localClassName.onCreate")
-        var btn = findViewById<Button>(R.id.buttonSecondActivity)
-        btn.setOnClickListener {
+
+        binding.buttonSecondActivity.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
-        btn = findViewById<Button>(R.id.buttonDialActivity)
-        btn.setOnClickListener {
+
+        binding.buttonDialActivity.setOnClickListener {
             val implicitIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:114"))
             startActivity(implicitIntent)
         }
-        btn = findViewById<Button>(R.id.buttonThirdActivity)
-        btn.setOnClickListener {
+
+        binding.buttonThirdActivity.setOnClickListener {
             val intent = Intent(this, ThirdActivity::class.java)
             intent.putExtra("UserDefinedExtra", "Hello")
             startActivityForResult(intent, request_code)
+        }
+
+        // ViewModel
+        viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
+        binding.textViewCount.text = getString(R.string.count_in_activity, count)
+        binding.textViewCountViewmodel.text = getString(R.string.count_in_ViewModel, viewModel.count)
+        viewModel.countLivedata.observe(this) {
+            binding.textViewLivedata.text = getString(R.string.count_in_ViewModel_LiveData, it)
+        }
+
+        binding.buttonIncr.setOnClickListener {
+            count++
+            viewModel.increaseCount()
+            binding.textViewCount.text = getString(R.string.count_in_activity, count)
+            binding.textViewCountViewmodel.text = getString(R.string.count_in_ViewModel, viewModel.count)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode != request_code || data == null) return
-        val msg = data.getStringExtra("ResultString") ?: ""
-        Snackbar.make(findViewById(R.id.buttonThirdActivity), "ActivityResult:$resultCode $msg", Snackbar.LENGTH_SHORT).show()
-        Log.i(TAG, "ActivityResult:$resultCode $msg")
+        if (requestCode == request_code) {
+            val msg = data?.getStringExtra("ResultString") ?: ""
+            Snackbar.make(binding.root, "ActivityResult:$resultCode $msg", Snackbar.LENGTH_SHORT).show()
+            Log.i(TAG, "ActivityResult:$resultCode $msg")
+        }
     }
 
     override fun onStart() {
---
+++ examples/activity_intent/app/src/main/java/com/example/activity_intent/MyViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.activity_intent
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyViewModel : ViewModel() {
+    var count = 0
+    val countLivedata: MutableLiveData<Int> = MutableLiveData<Int>()
+
+    init {
+        countLivedata.value = 0
+        Log.i("MyViewModel", "MyViewModel created!")
+    }
+
+    fun increaseCount() {
+        count++
+        countLivedata.value = count
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("MyViewModel", "MyViewModel destroyed!")
+    }
+}
