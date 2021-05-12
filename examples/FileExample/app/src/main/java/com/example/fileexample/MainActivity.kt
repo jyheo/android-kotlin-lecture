92f730b5e0464b64f222aed63ab7fc97c55d5a8a
Junyoung Heo <jyheo0@gmail.com>
2021-05-12 22:28:58 +0900

    add preference

--- examples/FileExample/app/src/main/java/com/example/fileexample/MainActivity.kt
+++ examples/FileExample/app/src/main/java/com/example/fileexample/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.fileexample
 
 import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.edit
 import androidx.fragment.app.DialogFragment
+import androidx.preference.PreferenceManager
 import com.example.fileexample.databinding.ActivityMainBinding
 import java.io.File
 
@@ -20,41 +25,75 @@ class ShowValueDialog(private val msg: String) : DialogFragment() {
 }
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-    private val vmInternal by viewModels<MyViewModel> {
-        MyViewModelFatory(File(filesDir, "appfile.txt"))
-    }
-
-    private val vmExternal by viewModels<MyViewModel> {
-        MyViewModelFatory(File(getExternalFilesDir(null), "appfile.txt"))
-    }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val viewModel by viewModels<MyViewModel> { MyViewModelFactory(this) }
+    private val pref by lazy { getSharedPreferences("MY-SETTINGS", 0) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.buttonWriteIn.setOnClickListener {
-            vmInternal.value = binding.editText.text.toString()
+            viewModel.valueInternal = binding.editText.text.toString()
         }
 
         binding.buttonReadIn.setOnClickListener {
-            ShowValueDialog(vmInternal.value).show(supportFragmentManager, "ShowValueDialog")
+            ShowValueDialog(viewModel.valueInternal).show(supportFragmentManager, "ShowValueDialog")
         }
 
         binding.buttonWriteExt.setOnClickListener {
-            vmExternal.value = binding.editText.text.toString()
+            viewModel.valueExternal = binding.editText.text.toString()
 
         }
         binding.buttonReadExt.setOnClickListener {
-            ShowValueDialog(vmExternal.value).show(supportFragmentManager, "ShowValueDialog")
+            ShowValueDialog(viewModel.valueExternal).show(supportFragmentManager, "ShowValueDialog")
         }
+
+        binding.buttonWritePref.setOnClickListener {
+            pref.edit {
+                putString("key", binding.editText.text.toString())
+
+                apply()
+            }
+        }
+
+        binding.buttonReadPref.setOnClickListener {
+            ShowValueDialog(pref.getString("key", "") ?: "").show(supportFragmentManager, "ShowValueDialog")
+        }
+
+        displaySettings()
     }
 
-    private val isExternalStorageMounted: Boolean
-        get() {
-            val state = Environment.getExternalStorageState()
-            return Environment.MEDIA_MOUNTED == state
+    private fun displaySettings() {
+        val settings = PreferenceManager.getDefaultSharedPreferences(this)
+        val signature = settings.getString("signature", "")
+        val reply = settings.getString("reply", "")
+        val sync = settings.getBoolean("sync", false)
+        val attachment = settings.getBoolean("attachment", false)
+        val str = """signature: $signature
+reply: $reply
+sync: $sync
+attachment: $attachment
+"""
+        binding.textViewSettings.text = str
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> startActivityForResult(Intent(this, SettingsActivity::class.java), 0)
+            else -> return super.onOptionsItemSelected(item)
         }
+        return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0)
+            displaySettings()
+    }
 }
--- examples/FileExample/app/src/main/java/com/example/fileexample/MyViewModel.kt
+++ examples/FileExample/app/src/main/java/com/example/fileexample/MyViewModel.kt
@@ -1,30 +1,58 @@
 package com.example.fileexample
 
+import android.content.Context
+import android.os.Environment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import java.io.File
 import java.lang.IllegalArgumentException
 
-class MyViewModel(private val file: File) : ViewModel() {
-    var value: String = readValue()
+class MyViewModel(context: Context) : ViewModel() {
+    private val fileInternal = File(context.filesDir, "appfile.txt")
+    private val fileExternal =
+        if (isExternalStorageMounted)
+            File(context.getExternalFilesDir(null), "appfile.txt")
+        else
+            fileInternal
+
+    var valueInternal: String = readValue(fileInternal)
         set(v) {
             field = v
-            writeValue(v)
+            writeValue(fileInternal, v)
         }
 
-    private fun readValue() : String {
-        return file.readText(Charsets.UTF_8)
+    var valueExternal: String = readValue(fileExternal)
+        set(v) {
+            field = v
+            writeValue(fileExternal, v)
+        }
+
+    private fun readValue(file: File) : String {
+        return try {
+            println("$file")
+            // Internal Storage - /data/user/0/com.example.fileexample/files/appfile.txt
+            // External Storage - /storage/emulated/0/Android/data/com.example.fileexample/files/appfile.txt
+            file.readText(Charsets.UTF_8)
+        } catch (e: Exception) {
+            ""
+        }
     }
 
-    private fun writeValue(value: String) {
+    private fun writeValue(file: File, value: String) {
         file.writeText(value, Charsets.UTF_8)
     }
+
+    private val isExternalStorageMounted: Boolean
+        get() {
+            val state = Environment.getExternalStorageState()
+            return state == Environment.MEDIA_MOUNTED
+        }
 }
 
-class MyViewModelFatory(private val file: File) : ViewModelProvider.Factory {
+class MyViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(MyViewModel::class.java))
-            MyViewModel(file) as T
+            MyViewModel(context) as T
         else
             throw IllegalArgumentException()
     }
