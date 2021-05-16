3d480a3716d3d67c2e8cbf9675328db639f618e9
Junyoung Heo <jyheo0@gmail.com>
2021-05-16 14:40:14 +0900

    foreground service

--- examples/BackgroundTask/app/src/main/java/com/example/backgroundtask/MainActivity.kt
+++ examples/BackgroundTask/app/src/main/java/com/example/backgroundtask/MainActivity.kt
@@ -1,58 +1,109 @@
 package com.example.backgroundtask
 
+import android.app.Notification
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.backgroundtask.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var scope: CoroutineScope
+
+    val channelID = "default"
+
+    private fun updateNotification(id: Int, notification: Notification) {
+        NotificationManagerCompat.from(this).notify(id, notification)
+    }
+
+    private fun createNotification(progress: Int = 0) = NotificationCompat.Builder(this, channelID)
+        .setContentTitle("Downloading")
+        .setContentText("Downloading a file from a cloud")
+        .setSmallIcon(R.drawable.ic_baseline_cloud_download_24)
+        .setOnlyAlertOnce(true)  // importance 에 따라 알림 소리가 날 때, 처음에만 소리나게 함
+        .setProgress(100, progress, false)
+        .build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.buttonStartCo.setOnClickListener {
-            scope = CoroutineScope(Dispatchers.Default + Job()).apply {
-                launch {
-                    for (i in 1..10) {
-                        delay(1000)
-                        withContext(Dispatchers.Main) {
-                            binding.textView.text = "$i"
-                        }
-                    }
-                }
+            startCoroutine()
 
-                launch {
-                    for (i in 1..10) {
-                        delay(1000)
-                        println("#$i")
-                    }
-                }
-            }
         }
 
         binding.buttonStopCo.setOnClickListener {
-            scope.cancel()
-        }
-
-        binding.button.setOnClickListener {
-            Snackbar.make(it, "TEST", Snackbar.LENGTH_SHORT).show()
+            stopCoroutine()
         }
 
         binding.buttonStartService.setOnClickListener {
             Intent(this, MyService::class.java).also {
-                startService(it)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // Android 8.0
+                    startForegroundService(it) // must call startForground in a few seconds.
+                } else {
+                    startService(it)
+                }
             }
         }
 
-        binding.buttonStopService.setOnClickListener {
-            stopService(Intent(this, MyService::class.java))
+        binding.buttonStartedCount.setOnClickListener {
+            binding.textViewCount.text = "${myService?.startedCount}"
         }
 
+    }
+
+    private fun startCoroutine() {
+        scope = CoroutineScope(Dispatchers.Default + Job()).apply {
+            launch {
+                for (i in 1..10) {
+                    delay(1000)
+                    withContext(Dispatchers.Main) {
+                        binding.textView.text = "$i"
+                    }
+                }
+            }
+
+            launch {
+                for (i in 1..10) {
+                    updateNotification(1, createNotification(i*10))
+                    delay(1000)
+                    println("#$i")
+                }
+            }
+        }
+    }
+
+    private fun stopCoroutine() = scope.cancel()
+
+    private var myService: MyService? = null
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            myService = (service as MyService.LocalBinder).getService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            myService = null
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Intent(this, MyService::class.java).also {
+            bindService(it, serviceConnection, BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(serviceConnection)
     }
 }
--- examples/BackgroundTask/app/src/main/java/com/example/backgroundtask/MyService.kt
+++ examples/BackgroundTask/app/src/main/java/com/example/backgroundtask/MyService.kt
@@ -1,39 +1,83 @@
 package com.example.backgroundtask
 
-import android.app.Service
+import android.app.*
 import android.content.Intent
+import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.coroutines.*
 
 class MyService : Service() {
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    private val binder = LocalBinder()
+
+    inner class LocalBinder : Binder() {
+        // Return this instance of LocalService so clients can call public methods
+        fun getService() = this@MyService
     }
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+
+    var startedCount = 0
+        private set
 
     override fun onCreate() {
         super.onCreate()
         println("MyService:onCreate")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // Android 8.0
+            createNotificationChannel()
+        }
     }
-
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         println("MyService:onStartCommand $startId")
+        startedCount++
 
-        CoroutineScope(Dispatchers.IO).apply {
+        startForeground(notificationID, createNotification())
+
+        CoroutineScope(Dispatchers.IO + Job()).apply {
             launch {
-                for (i in 1..5) {
+                for (i in 1..10) {
                     println("in service $startId#$i")
+                    updateNotification(notificationID, createNotification(i*10))
                     delay(1000)
                 }
                 stopSelf(startId)
             }
         }
 
-        return super.onStartCommand(intent, flags, startId)
+        return super.onStartCommand(intent, flags, startId) // START_STICKY, NOT_STICKY, REDELIVERY_INTENT
     }
 
     override fun onDestroy() {
         super.onDestroy()
         println("MyService:onDestroy")
     }
+
+    private val channelID = "default"
+    private val notificationID = 1
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(channelID, "default channel",
+            NotificationManager.IMPORTANCE_DEFAULT)
+        channel.description = "description text of this channel."
+        NotificationManagerCompat.from(this).createNotificationChannel(channel)
+    }
+
+    private fun updateNotification(id: Int, notification: Notification) {
+        NotificationManagerCompat.from(this).notify(id, notification)
+    }
+
+    private fun createNotification(progress: Int = 0) = NotificationCompat.Builder(this, channelID)
+        .setContentTitle("Downloading")
+        .setContentText("Downloading a file from a cloud")
+        .setSmallIcon(R.drawable.ic_baseline_cloud_download_24)
+        .setOnlyAlertOnce(true)  // importance 에 따라 알림 소리가 날 때, 처음에만 소리나게 함
+        .setProgress(100, progress, false)
+        .build()
 
 }
