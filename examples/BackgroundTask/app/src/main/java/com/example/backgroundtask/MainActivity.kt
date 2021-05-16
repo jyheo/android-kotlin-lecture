7c124503cc2f54cf60d15c49d7942a07442c0135
Junyoung Heo <jyheo0@gmail.com>
2021-05-16 14:41:51 +0900

    remove unnecessary notification code

--- examples/BackgroundTask/app/src/main/java/com/example/backgroundtask/MainActivity.kt
+++ examples/BackgroundTask/app/src/main/java/com/example/backgroundtask/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.backgroundtask
 
-import android.app.Notification
 import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
@@ -8,28 +7,12 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.example.backgroundtask.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var scope: CoroutineScope
-
-    val channelID = "default"
-
-    private fun updateNotification(id: Int, notification: Notification) {
-        NotificationManagerCompat.from(this).notify(id, notification)
-    }
-
-    private fun createNotification(progress: Int = 0) = NotificationCompat.Builder(this, channelID)
-        .setContentTitle("Downloading")
-        .setContentText("Downloading a file from a cloud")
-        .setSmallIcon(R.drawable.ic_baseline_cloud_download_24)
-        .setOnlyAlertOnce(true)  // importance 에 따라 알림 소리가 날 때, 처음에만 소리나게 함
-        .setProgress(100, progress, false)
-        .build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,7 +56,6 @@ class MainActivity : AppCompatActivity() {
 
             launch {
                 for (i in 1..10) {
-                    updateNotification(1, createNotification(i*10))
                     delay(1000)
                     println("#$i")
                 }
