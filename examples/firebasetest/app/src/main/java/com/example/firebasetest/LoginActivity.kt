72e4f911da61a71621c5fd907db29fa268784a8e
Junyoung Heo <jyheo0@gmail.com>
2021-07-08 11:01:06 +0900

    separate login from view

--- examples/firebasetest/app/src/main/java/com/example/firebasetest/LoginActivity.kt
+++ examples/firebasetest/app/src/main/java/com/example/firebasetest/LoginActivity.kt
@@ -6,34 +6,33 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firebasetest.databinding.ActivityLoginBinding
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        auth = Firebase.auth
-
         binding.login.setOnClickListener {
             val userEmail = binding.username.text.toString()
             val password = binding.password.text.toString()
-            auth.signInWithEmailAndPassword(userEmail, password)
-                .addOnCompleteListener(this) {
-                    if (it.isSuccessful) {
-                        startActivity(
-                            Intent(this, MainActivity::class.java))
-                        finish()
-                    } else {
-                        Log.w("LoginActivity", "signInWithEmail", it.exception)
-                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
-                    }
-                }
+            doLogin(userEmail, password)
         }
+    }
+
+    private fun doLogin(userEmail: String, password: String) {
+        Firebase.auth.signInWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    startActivity(
+                        Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Log.w("LoginActivity", "signInWithEmail", it.exception)
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
